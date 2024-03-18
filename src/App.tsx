94af1b445f38bf2cdd80864251465cce9aebc5bc
tamammabroukeh <tamammb367@@gmail.com>
2024-03-18 05:31:01 -0700

    Building a react-router-dom-project-learn-contribute-login-pages-and-some-features

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+
+const App = () => {
+  return (
+    <>
+      {/* <RouterProvider router={router} /> */}
+      <h1 className="text-3xl mt-10 font-bold underline">Hello world!</h1>
+    </>
+  );
+};
+
+export default App;
